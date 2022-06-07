0fbeeb95a8d0f74412c0f8e7d279071bd664e858
world29 <623p6236p@gmail.com>
2022-06-08 01:55:49 +0900

    webpack, React をインストール

---
+++ src/Hello.jsx
@@ -0,0 +1,5 @@
+﻿import React from "react";
+
+const Hello = () => <h1>Hello world!</h1>;
+
+export default Hello;
---
+++ src/app.js
@@ -0,0 +1,10 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./Hello";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Hello />
+  </React.StrictMode>,
+  document.getElementById("app")
+);
