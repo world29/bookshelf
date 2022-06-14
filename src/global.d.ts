b83d892e21ae4a7eb7aab3c4e9cd13b7d95a57f3
world29 <623p6236p@gmail.com>
2022-06-15 01:48:26 +0900

    ダイアログを追加

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+﻿// ネイティブ型の変更
+// https://typescript-jp.gitbook.io/deep-dive/type-system/lib.d.ts#neitibunative-typeswosuru
+
+// https://github.com/microsoft/TypeScript/issues/48267
+interface HTMLDialogElement {
+  open: boolean;
+  show: () => void;
+  showModal: () => void;
+  close: () => void;
+}
---
+++ src/renderer/Dialog.tsx
@@ -0,0 +1,42 @@
+﻿import React, { useCallback, useEffect, useRef } from "react";
+
+export interface DialogProps {
+  onClose: () => void;
+  open?: boolean;
+  children?: React.ReactNode;
+}
+
+export function Dialog(props: DialogProps) {
+  const dialogEl: React.MutableRefObject<HTMLDialogElement | null> =
+    useRef(null);
+
+  const showModal = useCallback(() => {
+    if (dialogEl.current && !dialogEl.current.open) {
+      dialogEl.current.showModal();
+    }
+  }, []);
+
+  const closeModal = useCallback(() => {
+    if (dialogEl.current) {
+      dialogEl.current.close();
+      props.onClose();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (props.open) {
+      showModal();
+    }
+  }, [dialogEl]);
+
+  return (
+    <div>
+      <dialog ref={dialogEl}>
+        {props.children}
+        <button type="button" onClick={closeModal}>
+          close
+        </button>
+      </dialog>
+    </div>
+  );
+}
