3d2e96bf393f1f0536baee886f24b1f1fe327a1c
world29 <623p6236p@gmail.com>
2023-03-31 16:29:00 +0900

    モーダルを追加

---
+++ src/renderer/common/Modal.tsx
@@ -0,0 +1,35 @@
+﻿import { ReactNode } from "react";
+import "../styles/Modal.css";
+
+type ModalProps = {
+  open: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+function Modal(props: ModalProps) {
+  const { open, onClose, children } = props;
+
+  const handleClickBackground = () => {
+    onClose();
+  };
+
+  const handleClickModalContent = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+  };
+
+  if (open) {
+    return (
+      <div id="overlay" onClick={handleClickBackground}>
+        <div id="content" onClick={handleClickModalContent}>
+          {children}
+          <button onClick={() => onClose()}>close</button>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default Modal;
