193d577cced5e000525f8593a8acb98ac40f6c9e
world29 <623p6236p@gmail.com>
2023-04-01 23:44:07 +0900

    使用していないモジュールの import を削除

--- src/renderer/features/editor/BookAddDialog.tsx
+++ src/renderer/features/editor/BookAddDialog.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState } from "react";
+﻿import React, { useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import Modal from "../../common/Modal";
