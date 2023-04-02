ac1d2fbec527bd4ff9b76858372a89fa123568fb
world29 <623p6236p@gmail.com>
2023-04-02 21:26:37 +0900

    リファクタリング：ファイル名を変更

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import store from "./app/store";
-import App from "./app";
+import App from "./App";
 import "./styles/index.css";
 
 const container = document.getElementById("app");
