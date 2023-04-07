81c37c4a0ff11c34fa4e393ca2c7835032d7b1e8
world29 <623p6236p@gmail.com>
2023-04-07 22:57:54 +0900

    メニューを追加

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,8 @@
-﻿import { app, BrowserWindow, session } from "electron";
+﻿import { app, BrowserWindow, Menu, session } from "electron";
 import { join } from "path";
 import setupAPIs from "./api";
 import db from "./database";
+import { createMenu } from "./menu";
 
 const reactDevTools = join(__dirname, "../externals/ReactDevTools");
 
@@ -15,6 +16,9 @@ const createWindow = () => {
   });
   setupAPIs(mainWindow);
   mainWindow.loadFile("index.html");
+
+  const menu = createMenu(mainWindow);
+  Menu.setApplicationMenu(menu);
 
   mainWindow.webContents.openDevTools();
 
---
+++ src/main/menu.ts
@@ -0,0 +1,30 @@
+﻿import { app, BrowserWindow, Menu, MenuItemConstructorOptions } from "electron";
+
+export function createMenu(mainWindow: BrowserWindow) {
+  const template: MenuItemConstructorOptions[] = [
+    {
+      label: "File",
+      submenu: [
+        {
+          label: "Settings",
+        },
+        {
+          type: "separator",
+        },
+        {
+          label: "Exit",
+          click: () => app.quit(),
+        },
+      ],
+    },
+    { role: "editMenu" },
+    { role: "viewMenu" },
+    { role: "windowMenu" },
+    { role: "help", submenu: [{ role: "about" }] },
+  ];
+
+  // macOS では "アプリメニュー" が必要
+  if (process.platform === "darwin") template.unshift({ role: "appMenu" });
+
+  return Menu.buildFromTemplate(template);
+}
