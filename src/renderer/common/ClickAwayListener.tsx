8e044a0aee66ce715fd84cd326ca566ad9cf2cd2
world29 <623p6236p@gmail.com>
2023-04-10 11:59:04 +0900

    ClickAwayListener を追加

---
+++ src/renderer/common/ClickAwayListener.tsx
@@ -0,0 +1,32 @@
+﻿import { ReactNode, useEffect, useRef } from "react";
+
+// コンポーネントの外側で発生したクリックをハンドリングする
+// https://stackoverflow.com/a/42234988
+function useOutsideClickHandler(
+  ref: React.RefObject<HTMLDivElement>,
+  onClick: () => void
+) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        onClick();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+function ClickAwayListener(props: {
+  onClick: () => void;
+  children: ReactNode;
+}) {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  useOutsideClickHandler(wrapperRef, props.onClick);
+
+  return <div ref={wrapperRef}>{props.children}</div>;
+}
+
+export default ClickAwayListener;
