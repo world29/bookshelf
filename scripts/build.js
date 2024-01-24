be3710736ee04576605c7fbe86e5cf567a103e39
world29 <623p6236p@gmail.com>
2024-01-25 00:31:11 +0900

    インストーラーの作成フローを変更

---
+++ scripts/build.js
@@ -0,0 +1,29 @@
+﻿const builder = require("electron-builder");
+const Platform = builder.Platform;
+
+// Let's get that intellisense working
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const options = {
+  directories: {
+    output: "release",
+  },
+  win: {
+    target: "nsis",
+  },
+};
+
+builder
+  .build({
+    targets: Platform.WINDOWS.createTarget(),
+    config: options,
+    prepackaged: "./out/bookshelf-win32-x64",
+  })
+  .then((result) => {
+    console.log(JSON.stringify(result));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
