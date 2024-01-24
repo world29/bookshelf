571ced6191f15c4fe0bcce7c03adfbafcee1c39d
world29 <623p6236p@gmail.com>
2024-01-24 18:58:27 +0900

    electron-winstaller によるインストーラ作成に対応

---
+++ scripts/create-installer.js
@@ -0,0 +1,17 @@
+﻿const electronInstaller = require("electron-winstaller");
+
+console.log("creating installer...");
+
+electronInstaller
+  .createWindowsInstaller({
+    appDirectory: "./out/bookshelf-win32-x64",
+    outputDirectory: "./out/dist/win32-x64",
+    authors: "world29",
+    exe: "bookshelf.exe",
+  })
+  .then(() => {
+    console.log("completed.");
+  })
+  .catch((e) => {
+    console.error(e.message);
+  });
